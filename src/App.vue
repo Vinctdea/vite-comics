7582e4b4754a0bd25c8b6f116bb417915ef077c9
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-07-08 22:22:25 +0200

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
+export default{
+  name:'app',
+  components:{
+    AppHeader,
+  }
+}
 </script>
 
 <template>
+  <AppHeader/>
   
 </template>
 
 <style lang="scss">
 @use './styles/general.scss' as *;
+@use './styles/partials/variables';
+@use './styles/partials/mixins';
 
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,41 @@
+<script>
+export default{
+    name: "AppHeader",
+}
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="../../public/img/dc-logo.png" alt="logo dc">
+            </div>
+            <div class="navigation">
+                <nav>
+                    <a href="#">characters</a>
+                   
+                </nav>
+            </div>
+        </div>
+    </header>
+
+</template>
+
+<style lang="scss" scoped>
+header{
+    height: 50px;
+    padding: 1rem;
+
+    .container{
+        width: 80%;
+        display: flex;
+        margin: 0 auto;
+        a{
+            text-decoration: none;
+            color: black;
+            text-transform: uppercase;
+        }
+    }
+}
+    
+</style>
